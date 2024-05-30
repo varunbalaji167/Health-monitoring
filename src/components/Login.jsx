829c68be4a47a7f6f0f68490799cdf9f14a72a6c
varunbalaji167 <157891005+varunbalaji167@users.noreply.github.com>
2024-05-30 18:49:36 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,10 +4,11 @@ import {faNotesMedical,faUser,faLock} from '@fortawesome/free-solid-svg-icons'
 import './Login.css'
 
 const Login = () => {
-  return (<center>
+  return (
+    <center>
     <div className="logo"><h1><FontAwesomeIcon icon={faNotesMedical} /></h1></div>
     <div className="Container">
-    <div className="title"><h1>LogIn</h1></div>
+    <div className="title"><h1>Login</h1></div>
     <div className="containr inp">
         <div className="inputBox">
           <input type="text" placeholder="Username" name="Username"></input>
@@ -18,11 +19,11 @@ const Login = () => {
           <FontAwesomeIcon icon={faLock} />
         </div>
         <div className="rem-for">
-          <div className="rem"><input type="checkbox" id="remember"></input>Remember be</div>
-          <a href="/forgetpassword" id="forgetPassword" >forget password</a>
+          <div className="rem"><input type="checkbox" id="remember"></input>Remember me</div>
+          <a href="/forgotpassword" id="forgotPassword" >Forgot Password?</a>
         </div>
-        <button class="btn btn-primary" type="button">LogIn</button>
-        <p className='existAccount'>Don't have an account? <a href='/signup' className='linkLogin'>Register</a></p>
+        <button class="btn btn-primary" type="button">Login</button>
+        <p className='existAccount'>Don't have an account? <a href='/signup' className='linkLogin'>Register Here!</a></p>
     </div></div>
     </center>
   )
