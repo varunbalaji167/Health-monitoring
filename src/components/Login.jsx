ae90c4810fae7bb041fa803e62c501f2d1fbf6ff
varunbalaji167 <varunbalaji167@gmail.com>
2024-06-01 09:39:51 +0530

    added background

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,79 @@
-import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faNotesMedical,faUser,faLock} from '@fortawesome/free-solid-svg-icons'
-import './Login.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faNotesMedical,
+  faUser,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Login.css";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Login = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const printdata=()=>{
-    
-    console.log(username,password);
-    alert("You are logged in!!")
-  }
-  return (<center>
-    <div className="logo"><h1><FontAwesomeIcon icon={faNotesMedical} /></h1></div>
-    <div className="Container">
-    <div className="title"><h1>Login</h1></div>
-    <div className="containr inp">
-        <div className="inputBox">
-          <input type="text" placeholder="Username" name="Username" value={username}onChange={(e)=>{setUsername(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faUser} />
+  const printdata = () => {
+    console.log(username, password);
+    alert("You are logged in!!");
+  };
+  return (
+    <center>
+      <div className="logo">
+        <h1>
+          <FontAwesomeIcon icon={faNotesMedical} />
+        </h1>
+      </div>
+      <div className="Container">
+        <div className="title">
+          <h1>Login</h1>
         </div>
-        <div className="inputBox">
-          <input type="password" placeholder="Password" name="Password" value={password}onChange={(e)=>{setPassword(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faLock} />
+        <div className="containr inp">
+          <div className="inputBox">
+            <input
+              type="text"
+              placeholder="Username"
+              name="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            ></input>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <div className="inputBox">
+            <input
+              type="password"
+              placeholder="Password"
+              name="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            ></input>
+            <FontAwesomeIcon icon={faLock} />
+          </div>
+          <div className="rem-for">
+            <div className="rem">
+              <input type="checkbox" id="remember"></input>Remember me
+            </div>
+            <Link to="/forgotpassword" id="forgotPassword">
+              Forgot Password?
+            </Link>
+          </div>
+          <button className="btn btn-primary" type="button" onClick={printdata}>
+            Login
+          </button>
+          <p className="existAccount">
+            Don't have an account?{" "}
+            <Link to="/signup" className="linkLogin">
+              Register Here!
+            </Link>
+          </p>
         </div>
-        <div className="rem-for">
-          <div className="rem"><input type="checkbox" id="remember"></input>Remember me</div>
-          <Link to="/forgotpassword" id="forgotPassword" >Forgot Password?</Link>
-        </div>
-        <button className="btn btn-primary" type="button" onClick={printdata}>Login</button>
-        <p className='existAccount'>Don't have an account? <Link to='/signup' className='linkLogin'>Register Here!</Link></p>
-    </div></div>
+      </div>
     </center>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,80 +1,137 @@
-import React from 'react'
-import './Signup.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faLock, faSuitcaseMedical, faUser,faNotesMedical} from '@fortawesome/free-solid-svg-icons'
-import { Link } from "react-router-dom"
-import { useState } from 'react'
-import Doctor from './Doctor'
-import Dataentry from './Dataentry'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import "./Signup.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLock,
+  faSuitcaseMedical,
+  faUser,
+  faNotesMedical,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Doctor from "./Doctor";
+import Dataentry from "./Dataentry";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [cpassword, setCpassword] = useState("")
-  const [email, setEmail] = useState("")
-  const [role, setRole] = useState("")
-  const navigate=useNavigate();
-  const checkpassword=()=>{
-    if(username !== "" && email !== "" && password !== ""){
-    if(password===cpassword ){
-      console.log(username,password,cpassword,email,role)
-      alert("Your account got created");
-      if(role==="doctor"){
-      navigate("/doctor")}
-      if(role==="nurse"){
-        navigate("/nurse")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
+  const checkpassword = () => {
+    if (username !== "" && email !== "" && password !== "") {
+      if (password === cpassword) {
+        console.log(username, password, cpassword, email, role);
+        alert("Your account got created");
+        if (role === "doctor") {
+          navigate("/doctor");
+        }
+        if (role === "nurse") {
+          navigate("/nurse");
+        }
       }
-    }}
-    else{
-      alert("Can't register and enter details properly")
-      console.log(username,password,cpassword,email,role)
-      
+    } else {
+      alert("Can't register and enter details properly");
+      console.log(username, password, cpassword, email, role);
     }
-  }
-  return (<><center>
-    <div className="logo"><h1><FontAwesomeIcon icon={faNotesMedical} /></h1></div>
-    <div className="Container">
-      <div className="title"><h1>Sign Up</h1></div>
-      <div className="containr inp">
-        <div className="inputBox">
-          <input type="text" placeholder="Username" name="Username" value={username}onChange={(e)=>{setUsername(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faUser} />
+  };
+  return (
+    <>
+      <center>
+        <div className="logo">
+          <h1>
+            <FontAwesomeIcon icon={faNotesMedical} />
+          </h1>
         </div>
-        <div className="inputBox">
-          <input type="password" placeholder="Password" name="Password" value={password}onChange={(e)=>{setPassword(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faLock} />
-        </div>
-        <div className="inputBox">
-          <input type="password" placeholder="Confirm Password" name="cPassword" value={cpassword}onChange={(e)=>{setCpassword(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faLock} />
-        </div>
-        <div className="inputBox">
-          <input type="text" placeholder="Email" name="Email" value={email}onChange={(e)=>{setEmail(e.target.value)}}></input>
-          <FontAwesomeIcon icon={faEnvelope} />
-        </div>
-        <div className="inputBox">
-            <select
-            className="occp"
-              name="Role"
-              value={role}
-              onChange={(e) => {
-                setRole(e.target.value);
-              }}
+        <div className="Container">
+          <div className="title">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="containr inp">
+            <div className="inputBox">
+              <input
+                type="text"
+                placeholder="Username"
+                name="Username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              ></input>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div className="inputBox">
+              <input
+                type="password"
+                placeholder="Password"
+                name="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              ></input>
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+            <div className="inputBox">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="cPassword"
+                value={cpassword}
+                onChange={(e) => {
+                  setCpassword(e.target.value);
+                }}
+              ></input>
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+            <div className="inputBox">
+              <input
+                type="text"
+                placeholder="Email"
+                name="Email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              ></input>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+            <div className="inputBox">
+              <select
+                className="occp"
+                name="Role"
+                value={role}
+                onChange={(e) => {
+                  setRole(e.target.value);
+                }}
+              >
+                <option value="">Occupation</option>
+                <option value="doctor">Doctor</option>
+                <option value="nurse">Nurse</option>
+              </select>
+              <FontAwesomeIcon icon={faSuitcaseMedical} />
+            </div>
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={checkpassword}
             >
-              <option value="">Occupation</option>
-              <option value="doctor">Doctor</option>
-              <option value="nurse">Nurse</option>
-            </select>
-          <FontAwesomeIcon icon={faSuitcaseMedical} />
+              Register
+            </button>
+            <p className="existAccount">
+              Already have an account?{" "}
+              <Link to="/login" className="linkLogin">
+                login
+              </Link>
+            </p>
+          </div>
         </div>
-        <button className="btn btn-primary" type="button" onClick={checkpassword}>Register</button>
-        <p className='existAccount'>Already have an account? <Link to='/login' className='linkLogin'>login</Link></p>
-      </div>
-    </div>
-    </center>
+      </center>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
